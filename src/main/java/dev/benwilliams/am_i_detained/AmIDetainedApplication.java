986ef10c1a7ace291b725e0cc3f453e27f23d338
Ben Williams <ben@benwilliams.dev>
2026-01-13 23:30:34 -0500

    Initial commit :police_car:

---
+++ src/main/java/dev/benwilliams/am_i_detained/AmIDetainedApplication.java
@@ -0,0 +1,13 @@
+package dev.benwilliams.am_i_detained;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmIDetainedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmIDetainedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/benwilliams/am_i_detained/AmIDetainedApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.benwilliams.am_i_detained;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmIDetainedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
